b17352bea2bf62962f6ac32c8e31c67ccc63bf74
nhuamani <nhuamani.c7@gmail.com>
2025-01-23 16:54:58 -0500

    se implementa tabla de material 1 forma

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,12 +1,16 @@
-import { Product } from './../../interfaces/product';
-import { Component, inject, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, inject, OnInit, signal, ViewChild, WritableSignal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Product } from './../../interfaces/product';
 import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatTableModule, MatPaginatorModule, MatIconModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -14,19 +18,37 @@ export class ProductListComponent implements OnInit {
 
   private productService = inject(ProductService)
 
-  product: Product[] = []
+  
+  products: WritableSignal<Product[]> = signal<Product[]>([]);
+  
+  displayedColumns: string[] = ['id', 'name', 'description', 'price', 'stock', 'actions'];
+  dataSource = new MatTableDataSource<Product>([]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
     this.productService.getProducts().subscribe({
-      next: (resp) => {
-        console.log('API response', resp);
-      },
-      error: (err) => {
-        console.error('API error', err);
-      },
-      complete: () => {
-        console.log('API request completed')
+      next: (products) => {
+        this.products.set(products);
+        this.updateTableData();
       }
     })
+  }
+
+  updateTableData() {
+    this.dataSource.data = this.products();
+    this.dataSource.paginator = this.paginator;
+  }
+
+  navigateToForm(id?: number): void {
+    console.log('Navigate to form');
+  }
+
+  deleteProduct(id?: number): void {
+    console.log('Delete product', id);
   }
 }
